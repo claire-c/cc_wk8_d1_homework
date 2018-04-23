9197fe2084c18d2fd4a4329bea2ba6dae5472853
Claire <cconnachan@gmail.com>
2018-04-23 20:29:35 +0100

    added wordCounterActivity method

--- app/src/main/java/com/cconnachan/wordcounter/WordCounterActivity.java
+++ app/src/main/java/com/cconnachan/wordcounter/WordCounterActivity.java
@@ -2,12 +2,28 @@ package com.cconnachan.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WordCounterActivity extends AppCompatActivity {
+
+    private EditText wordsEditText;
+    private TextView answerTextView;
+    private Button button;
+    private WordCounter wordCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_counter);
+    }
+
+    public void onButtonClicked(View button){
+        String wordsToCount = wordsEditText.getText().toString();
+        Log.d(getClass().toString(), "The words to count are " + wordsToCount);
+        answerTextView.setText(wordCounter.count(wordsToCount));
     }
 }
